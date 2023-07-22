a8571d75547c5fc4c974da40866934ad6d0c54c9
layanmoyura <layanmoyura@gmail.com>
2023-07-22 21:15:28 +0530

    initial GUI part

--- client.py
+++ client.py
@@ -2,9 +2,78 @@ import socket
 import threading
 import sys
 import os
+import tkinter as tk
+import tkinter.scrolledtext as tkst
 
 Host = 'localhost'
 Port = 1234  # Any port bethween 0 and 65535
+
+def add_message_to_message_box(message):
+    message_box.config(state=tk.NORMAL)
+    message_box.insert(tk.END, message + "\n")
+    message_box.config(state=tk.DISABLED)
+
+def connect_to_server():
+    try:
+        client.connect((Host, Port))
+        print("Connected to server on port: %s" % Port)
+        communicate_to_server(client)
+
+    except:
+        print("Connection failed. Error : " + str(sys.exc_info()))
+        sys.exit()
+    print("Connecting to server...")
+
+def send_message():
+    print("Sending message...")
+
+
+DARK_GREY = '#121212'
+MEDIUM_GREY = '#1F1824'
+OCEAN_BLUE = '#464EB8'
+WHITE = "white"
+FONT=('Helvetica', 17)
+BUTTON_FONT = ('Helvetica', 15)
+SMALL_FONT = ('Helvetica', 13)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+root = tk.Tk()
+root.geometry("600x600")
+root.title("Messenger Client")
+root.resizable(False,False)
+
+root.grid_rowconfigure(0, weight=1)
+root.grid_rowconfigure(1, weight=4)
+root.grid_rowconfigure(2, weight=1)
+
+top_frame = tk.Frame(root, width=600, height=100, bg=DARK_GREY)
+top_frame.grid(row=0, column=0, sticky=tk.NSEW)
+
+middle_frame = tk.Frame(root, width=600, height=400, bg=MEDIUM_GREY)
+middle_frame.grid(row=1, column=0, sticky=tk.NSEW)
+
+bottom_frame = tk.Frame(root, width=600, height=100, bg=DARK_GREY)
+bottom_frame.grid(row=2, column=0, sticky=tk.NSEW)
+
+username_label = tk.Label(top_frame, text="Enter Username:", bg=DARK_GREY, fg=WHITE, font=FONT)
+username_label.pack(side=tk.LEFT, padx=10)
+
+username_textbox = tk.Entry(top_frame, font=FONT, bg=DARK_GREY, fg=WHITE, width=23)
+username_textbox.pack(side=tk.LEFT)
+
+username_button = tk.Button(top_frame, text="Join", font=BUTTON_FONT, bg=OCEAN_BLUE, fg=WHITE, command=connect_to_server)
+username_button.pack(side=tk.LEFT, padx=15)
+
+message_textbox = tk.Entry(bottom_frame, font=FONT, bg=MEDIUM_GREY, fg=WHITE, width=38)
+message_textbox.pack(side=tk.LEFT, padx=10)
+ 
+message_button = tk.Button(bottom_frame, text="Send", font=BUTTON_FONT, bg=OCEAN_BLUE, fg=WHITE, command=send_message)
+message_button.pack(side=tk.LEFT, padx=10)
+
+message_box = tkst.ScrolledText(middle_frame, bg=MEDIUM_GREY, fg=WHITE, font=SMALL_FONT, width=67, height=23)
+message_box.pack(side=tk.DISABLED)
+message_box.pack(side=tk.TOP)
 
 
 def listen_for_messages(client):
@@ -79,15 +148,11 @@ def communicate_to_server(client):
 
 
 def main():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        client.connect((Host, Port))
-        print("Connected to server on port: %s" % Port)
-        communicate_to_server(client)
 
-    except:
-        print("Connection failed. Error : " + str(sys.exc_info()))
-        sys.exit()
+    root.mainloop() 
+
+    
+   
 
 
 if __name__ == '__main__':
